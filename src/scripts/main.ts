35f0b1eb974d8bd18f01ca51f1b2821f84ee3a07
mathxlee <math.lee@qq.com>
2020-04-17 18:51:46 +0800

    feat: change page style

---
+++ src/scripts/main.ts
@@ -0,0 +1,7 @@
+import '../../node_modules/normalize.css/normalize.css';
+import '../../node_modules/@vipui/vipui/dist/css/vipui.css';
+import '../../node_modules/font-awesome/css/font-awesome.css';
+
+import '../styles/rainbow.css';
+import '../styles/monokai.css';
+import '../styles/main.scss';
